3180e769523b53355867864c5316fecac22616ca
Jaime Alvarez <jrralvarez@gmail.com>
2021-05-03 21:43:45 -0400

    installed cors to allow other servers to load the resources.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 const collections = require('./routes/collections');
+const cors = require('cors');
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/collections', collections);
 
